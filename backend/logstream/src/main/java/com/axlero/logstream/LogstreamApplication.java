f973af9575c0412286f14ea1d43f4f6785ca1d8c
Shifa Ahmed <ahmedshifa59@gmail.com>
2026-07-18 21:18:27 +0530

    initialize Spring Boot backend

---
+++ backend/logstream/src/main/java/com/axlero/logstream/LogstreamApplication.java
@@ -0,0 +1,13 @@
+package com.axlero.logstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogstreamApplication.class, args);
+	}
+
+}
