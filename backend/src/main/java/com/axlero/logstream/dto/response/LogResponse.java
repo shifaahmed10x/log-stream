97c1568fc872412c010ee6d79641e24f72e9765e
shifaahmed10x <ahmedshifa59@gmail.com>
2026-07-19 11:21:28 +0530

    feat(dto): add Log request and response DTOs

--- backend/src/main/java/com/axlero/logstream/dto/response/LogResponse.java
+++ backend/src/main/java/com/axlero/logstream/dto/response/LogResponse.java
@@ -1,4 +1,34 @@
 package com.axlero.logstream.dto.response;
 
+import com.axlero.logstream.enums.LogLevel;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LogResponse {
+
+    private Long id;
+
+    private String applicationName;
+
+    private String serviceName;
+
+    private String hostName;
+
+    private LogLevel logLevel;
+
+    private String message;
+
+    private String loggerName;
+
+    private String threadName;
+
+    private LocalDateTime timestamp;
+
+    private LocalDateTime createdAt;
 }
