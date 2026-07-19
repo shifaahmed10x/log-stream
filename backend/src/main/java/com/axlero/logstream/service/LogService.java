c19d827b896e2f273313154981f37449a55e032a
shifaahmed10x <ahmedshifa59@gmail.com>
2026-07-19 11:37:47 +0530

    feat(service): create LogService interface

--- backend/src/main/java/com/axlero/logstream/service/LogService.java
+++ backend/src/main/java/com/axlero/logstream/service/LogService.java
@@ -1,4 +1,16 @@
 package com.axlero.logstream.service;
 
-public class LogService {
+import com.axlero.logstream.dto.request.LogRequest;
+import com.axlero.logstream.dto.response.LogResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface LogService {
+    LogResponse saveLog(LogRequest request);
+    List<LogResponse> getAllLogs();
+    LogResponse getLogById(Long id);
+    void deleteLog(Long id);
+
 }
