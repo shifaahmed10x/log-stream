7322b703a828e821b7319868009e260073da11b8
shifaahmed10x <ahmedshifa59@gmail.com>
2026-07-23 10:01:08 +0530

    feat(exception): add ResourceNotFoundException

--- backend/src/main/java/com/axlero/logstream/service/impl/LogServiceImpl.java
+++ backend/src/main/java/com/axlero/logstream/service/impl/LogServiceImpl.java
@@ -2,16 +2,17 @@ package com.axlero.logstream.service.impl;
 
 import com.axlero.logstream.dto.request.LogRequest;
 import com.axlero.logstream.dto.response.LogResponse;
+import com.axlero.logstream.entity.Log;
+import com.axlero.logstream.mapper.LogMapper;
 import com.axlero.logstream.repository.LogRepository;
 import com.axlero.logstream.service.LogService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class LogServiceImpl implements LogService {
-     // here we had use Constructor Injection insta of @Autowired
+     // here we had use Constructor Injection instead of @Autowired
     private final LogRepository logRepository;
 
     public LogServiceImpl(LogRepository logRepository){
@@ -30,15 +31,15 @@ public class LogServiceImpl implements LogService {
 
     @Override
     public List<LogResponse> getAllLogs() {
-        return List.of();
+         return logRepository.findAll()
+                .stream()
+                .map(LogMapper::toResponse) // log -> LogMapper.toResponse(log
+                .toList();
     }
 
     @Override
     public LogResponse getLogById(Long id) {
-        return return logRepository.findAll()
-                .stream()
-                .map(LogMapper::toResponse) // log -> LogMapper.toResponse(log
-                .toList();
+return null ;
     }
 
     @Override
