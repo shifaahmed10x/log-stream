04d6e1e3c8a41d36c0c96317083aeaffae912308
shifaahmed10x <ahmedshifa59@gmail.com>
2026-07-23 10:13:07 +0530

    feat(service): implement getLogById operation

--- backend/src/main/java/com/axlero/logstream/service/impl/LogServiceImpl.java
+++ backend/src/main/java/com/axlero/logstream/service/impl/LogServiceImpl.java
@@ -3,12 +3,15 @@ package com.axlero.logstream.service.impl;
 import com.axlero.logstream.dto.request.LogRequest;
 import com.axlero.logstream.dto.response.LogResponse;
 import com.axlero.logstream.entity.Log;
+import com.axlero.logstream.exception.ResourceNotFoundException;
 import com.axlero.logstream.mapper.LogMapper;
 import com.axlero.logstream.repository.LogRepository;
 import com.axlero.logstream.service.LogService;
 import org.springframework.stereotype.Service;
 
+import java.lang.module.ResolutionException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LogServiceImpl implements LogService {
@@ -39,7 +42,11 @@ public class LogServiceImpl implements LogService {
 
     @Override
     public LogResponse getLogById(Long id) {
-return null ;
+
+       Log log = logRepository.findById(id)
+               .orElseThrow(()-> new ResourceNotFoundException("Log not found with id : "+ id));
+
+       return  LogMapper.toResponse(log);
     }
 
     @Override
