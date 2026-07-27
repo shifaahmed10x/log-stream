f6d13a4e8d303a30e67894cd3ccd8c062609974d
shifaahmed10x <ahmedshifa59@gmail.com>
2026-07-27 22:16:55 +0530

    feat(controller): add create log endpoint

--- backend/src/main/java/com/axlero/logstream/controller/LogController.java
+++ backend/src/main/java/com/axlero/logstream/controller/LogController.java
@@ -1,6 +1,13 @@
 package com.axlero.logstream.controller;
 
+import com.axlero.logstream.dto.request.LogRequest;
+import com.axlero.logstream.dto.response.LogResponse;
 import com.axlero.logstream.service.LogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +18,11 @@ public class LogController {
     private final LogService logService;
     public LogController(LogService logService) {
         this.logService = logService;
+    }
+
+    @PostMapping
+    public ResponseEntity<LogResponse>  saveLog (@RequestBody LogRequest logRequest){
+        LogResponse response = logService.saveLog(logRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
