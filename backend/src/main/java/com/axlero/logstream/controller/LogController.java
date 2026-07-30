932dada95eca861635d86424c3b36fa0b1934454
Shivam Kasaudhan <skg752581@gmail.com>
2026-07-30 12:45:29 +0530

    Add Swagger documentation and request validation

--- backend/src/main/java/com/axlero/logstream/controller/LogController.java
+++ backend/src/main/java/com/axlero/logstream/controller/LogController.java
@@ -3,6 +3,10 @@ package com.axlero.logstream.controller;
 import com.axlero.logstream.dto.request.SearchRequest;
 import com.axlero.logstream.dto.response.SearchResponse;
 import com.axlero.logstream.service.LogService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +21,16 @@ public class LogController {
     public LogController(LogService logService) {
         this.logService = logService;
     }
+    @Operation(
+            summary = "Search Logs",
+            description = "Search logs using keyword, filters, sorting and pagination"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode="200", description= "Logs retrieved successfully"),
+            @ApiResponse(responseCode="400",description ="Invalid search request")
+    })
     @PostMapping("/search")
-    public SearchResponse searchLogs(@RequestBody SearchRequest request){
+    public SearchResponse searchLogs(@Valid @RequestBody SearchRequest request){
         return logService.searchLogs(request);
     }
 }
--- backend/src/main/java/com/axlero/logstream/dto/request/SearchRequest.java
+++ backend/src/main/java/com/axlero/logstream/dto/request/SearchRequest.java
@@ -1,5 +1,7 @@
 package com.axlero.logstream.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -15,8 +17,12 @@ public class SearchRequest {
     private String threadName;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
+    @Min(value = 0, message = "page number cannot be negative")
     private Integer page = 0;
+    @Min(value = 1, message = "minimum page size 1")
+    @Max(value = 10, message = "maxmimum page size 10")
     private Integer pageSize = 10;
     private String sortBy = "timestamp";
     private String sortDirection = "desc";
+
 }
